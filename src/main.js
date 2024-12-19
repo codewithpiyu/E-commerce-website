2f351ebd49500f2e32635c3ebbd7db5a64ff2312
codewithpiyu <herballife.priyankanaik@gmail.com>
2024-12-19 15:16:20 +0530

    update all file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-import './style.css'
+import './style.css';
+import products from "../api/products.json";
+import { showProductContanier } from '../src/homeProductCard';
+
+// call the function to display all the product as card and input takes array
+
+showProductContanier(products);
